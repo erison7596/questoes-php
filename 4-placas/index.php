3f957ac169b6b85930983709ec98dccea16cf569
Erison Cavalcante <erison7596e@gmail.com>
2021-08-23 20:40:03 -0300

    4 questão lista (atividade para php.pdf)

---
+++ 4-placas/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$total=(100*2)/2;
+//será dessa forma pq quadrangular tem 2 metros de cada lado e o muro tem 100 por 4
+//ai seria 100 placas, pq daria 50 e 50, que daria 100 por 4
+echo "serão preciso ".$total." placas";
+
+?>
