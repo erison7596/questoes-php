bf1638d83f5b332e0314f2d3a92ac5f3d75e9b72
Erison Cavalcante <erison7596e@gmail.com>
2021-08-23 20:23:32 -0300

    3 questão lista(atividade para php.pdf)

---
+++ 3-carroWayne/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "Indo no carro Modelo morcego 1.0, ele gastou: $".number_format((((295/16)*0.75)+300),2);
+echo "<br>Indo no carro Modelo voador 2.0, ele gastou: $".number_format((((295/11)*0.75)+500),2);
+?>
